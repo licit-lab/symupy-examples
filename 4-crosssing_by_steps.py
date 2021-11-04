1181f4c5698c4bf4c62779c710f488c14f80adfc
Andres Ladino <aladinoster@gmail.com>
2021-11-04 11:41:59 +0100

    🚧 Crossing single vehicle (rerouting)

--- 4-crosssing_by_steps.py
+++ 4-crosssing_by_steps.py
@@ -5,7 +5,7 @@ from symupy.runtime.api import Simulator, Simulation
 from ctypes import c_double, c_int, c_char_p
 
 # https://gist.github.com/aladinoster/c477b37bb25ddc18c8d24229871d259e
-LIBRARY_PATH = "/Users/andresladino/Documents/01-Code/02-Python/research/control-routing/symuflow/build/src/libSymuFlow.dylib"
+LIBRARY_PATH = "/Users/andresladino/Documents/01-Code/11-Licit/symuvia/symuflow/build/src/libSymuFlow.dylib"
 
 scenario = os.path.join(
     os.getcwd(), "network", "Crossing", "crossing_singlevehicle.xml",
@@ -17,35 +17,19 @@ simulator.register_simulation(scenario)
 
 if __name__ == "__main__":
 
+    REROUTE = True
+
     with simulator as s:
-        simulator._Simulator__library.SymGetVehicleAcc.restype = c_double
-        simulator._Simulator__library.SymGetVehicleSpeed.restype = c_double
-        simulator._Simulator__library.SymGetVehicleLink.restype = c_char_p
-        simulator._Simulator__library.SymGetVehicleX.restype = c_double
-        simulator._Simulator__library.SymGetVehicleY.restype = c_double
 
         while s.do_next:
+
+            if s.simulationstep == 2 and REROUTE:
+                SNEWROUTE = "Link_A Link_C"
+                output = s.drive_vehicle_new_route(0, SNEWROUTE)
+                print(output)
+
+            # print(s.vehicles)
+
+            print(f"\tLink: {s.get_vehicle_link(0)}")
+
             s.run_step()
-
-            # Retrieve original query
-            # s.request.query
-
-            # # Retreive vehicle data formated as dictionaries
-            # s.request.get_vehicle_data()
-
-            # # Retreive data as a vehicle list (printed as dataframe)
-            print(s.vehicles)
-
-            if s.simulationstep == 2:
-                snewroute = "Link_A Link_C"
-                print(
-                    s._Simulator__library.SymAlterRouteEx(
-                        0, snewroute.encode("UTF8")
-                    )
-                )
-
-            print(
-                f"\tLink: {s._Simulator__library.SymGetVehicleLink(c_int(0))}"
-            )
-            print(f"\tX: {s._Simulator__library.SymGetVehicleX(c_int(0))}")
-            print(f"\tY: {s._Simulator__library.SymGetVehicleY(c_int(0))}\n")
